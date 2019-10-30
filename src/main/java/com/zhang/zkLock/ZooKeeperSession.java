6d9085cc2677ef42c23d232e95d8516be4eed812
zhangguowne <zhangguowen@tallsafe.com>
2019-10-30 09:39:57 +0800

    Initial commit

---
+++ src/main/java/com/zhang/zkLock/ZooKeeperSession.java
@@ -0,0 +1,142 @@
+package com.zhang.zkLock;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class ZooKeeperSession {
+
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    private ZooKeeper zookeeper;
+
+    private CountDownLatch latch;
+
+    public ZooKeeperSession() {
+        try {
+            this.zookeeper = new ZooKeeper("192.168.31.187:2181,192.168.31.19:2181,192.168.31.227:2181", 50000, new ZooKeeperWatcher());
+            try {
+                connectedSemaphore.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("ZooKeeper session established......");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取分布式锁
+     *
+     * @param productId
+     */
+    public Boolean acquireDistributedLock(Long productId,long waitTime) {
+        String path = "/product-lock-" + productId;
+
+        try {
+            zookeeper.create(path, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            return true;
+        } catch (Exception e) {
+            while (true) {
+                try {
+                    // 相当于是给node注册一个监听器，去看看这个监听器是否存在
+                    Stat stat = zookeeper.exists(path, true);
+
+                    if (stat != null) {
+                        this.latch = new CountDownLatch(1);
+                        this.latch.await(waitTime, TimeUnit.MILLISECONDS);
+                        this.latch = null;
+                    }
+                    zookeeper.create(path, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+                    return true;
+                } catch (Exception ee) {
+                    continue;
+                }
+            }
+
+        }
+
+    }
+
+    /**
+     * 释放掉一个分布式锁
+     *
+     * @param productId
+     */
+    public void releaseDistributedLock(Long productId) {
+        String path = "/product-lock-" + productId;
+        try {
+            zookeeper.delete(path, -1);
+            System.out.println("release the lock for product[id=" + productId + "]......");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 建立zk session的watcher
+     *
+     * @author bingo
+     * @since 2018/11/29
+     *
+     */
+    private class ZooKeeperWatcher implements Watcher {
+
+        public void process(WatchedEvent event) {
+            System.out.println("Receive watched event: " + event.getState());
+
+            if (Event.KeeperState.SyncConnected == event.getState()) {
+                connectedSemaphore.countDown();
+            }
+
+            if (latch != null) {
+                latch.countDown();
+            }
+        }
+
+    }
+
+    /**
+     * 封装单例的静态内部类
+     *
+     * @author bingo
+     * @since 2018/11/29
+     *
+     */
+    private static class Singleton {
+
+        private static ZooKeeperSession instance;
+
+        static {
+            instance = new ZooKeeperSession();
+        }
+
+        public static ZooKeeperSession getInstance() {
+            return instance;
+        }
+
+    }
+
+    /**
+     * 获取单例
+     *
+     * @return
+     */
+    public static ZooKeeperSession getInstance() {
+        return Singleton.getInstance();
+    }
+
+    /**
+     * 初始化单例的便捷方法
+     */
+    public static void init() {
+        getInstance();
+    }
+
+
+
+}
